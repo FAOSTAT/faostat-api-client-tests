f9d2aa7c6ec4e4ef1a0599f31da605247c0b4f67
Kalimaha <guido.barbaglia@gmail.com>
2015-09-02 17:05:58 +0200

    Work in progress for the FAOSTAT API client.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,31 @@
-'use strict';
+(function () {
 
-module.exports = function (grunt) {
+    'use strict';
 
-    /* Project configuration. */
-    grunt.initConfig({
+    /*global module*/
+    module.exports = function (grunt) {
 
-        /* Plugin configuration. */
-        jsonschema_amd_restclient_generator: {
-            custom_options: {
-                options: {
-                    base_url: 'http://localhost:8080/faostat-api/v1.0/',
-                    output_name: 'FAOSTATClient'
+        /* Project configuration. */
+        grunt.initConfig({
+
+            /* Plugin configuration. */
+            jsonschema_amd_restclient_generator: {
+                custom_options: {
+                    options: {
+                        base_url: 'http://localhost:8080/api/v1.0/',
+                        output_name: 'FAOSTATClient'
+                    }
                 }
             }
-        }
 
-    });
+        });
 
-    /* Load NPM tasks. */
-    grunt.loadNpmTasks('grunt-jsonschema-amd-restclient-generator');
+        /* Load NPM tasks. */
+        grunt.loadNpmTasks('grunt-jsonschema-amd-restclient-generator');
 
-    /* Register task. */
-    grunt.registerTask('default', ['jsonschema_amd_restclient_generator']);
+        /* Register task. */
+        grunt.registerTask('default', ['jsonschema_amd_restclient_generator']);
 
-};
+    };
+
+}());
