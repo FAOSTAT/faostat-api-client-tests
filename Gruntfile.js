8523c7bd8d2dfffa98d82974e7cbbc4038006276
Kalimaha <guido.barbaglia@gmail.com>
2015-09-14 12:21:44 +0200

    Groups and domains test.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@
             jsonschema_amd_restclient_generator: {
                 custom_options: {
                     options: {
-                        base_url: 'http://localhost:8080/api/v1.0/',
+                        base_url: 'http://fenixapps2.fao.org/api/v1.0/',
                         output_name: 'FAOSTATClient'
                     }
                 }
