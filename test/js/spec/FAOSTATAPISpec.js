9032d6c68aa4c377ce25d2990cc71718560f7a6c
Kalimaha <guido.barbaglia@gmail.com>
2015-11-17 15:35:08 +0100

    Domains.

--- test/js/spec/FAOSTATAPISpec.js
+++ test/js/spec/FAOSTATAPISpec.js
@@ -7,11 +7,13 @@ define(['faostat-api-client'], function (FAOSTATAPIClient) {
         services = {
             abbreviations: null,
             groups: null,
+            domains: null,
             bulk_downloads: null
         },
         expected = {
             abbreviations: 100,
             groups: 15,
+            domains: 7,
             bulk_downloads: 10
         };
 
@@ -44,6 +46,20 @@ define(['faostat-api-client'], function (FAOSTATAPIClient) {
             });
             it('that returns ' + expected.groups + ' values', function () {
                 expect(services.groups.data.length).toEqual(expected.groups);
+            });
+        });
+
+        describe('has a service for Domains', function () {
+            beforeEach(function (done) {
+                c.domains({
+                    group_code: 'Q'
+                }).then(function (response) {
+                    services.domains = response;
+                    done();
+                });
+            });
+            it('that returns ' + expected.domains + ' values for the Q group', function () {
+                expect(services.domains.data.length).toEqual(expected.domains);
             });
         });
 
