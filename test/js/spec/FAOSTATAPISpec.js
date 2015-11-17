a2d46ad98c824160e2f167dd87b6b786bcf5071f
Kalimaha <guido.barbaglia@gmail.com>
2015-11-17 15:18:28 +0100

    Abbreviations. Groups.

--- test/js/spec/FAOSTATAPISpec.js
+++ test/js/spec/FAOSTATAPISpec.js
@@ -1,36 +1,51 @@
-/*global define, describe, it, expect*/
+/*global define, describe, it, expect, beforeEach*/
 define(['faostat-api-client'], function (FAOSTATAPIClient) {
 
     'use strict';
 
-    var c = new FAOSTATAPIClient({});
+    var c = new FAOSTATAPIClient({}),
+        services = {
+            abbreviations: null,
+            groups: null,
+            bulk_downloads: null
+        },
+        expected = {
+            abbreviations: 100,
+            groups: 15,
+            bulk_downloads: 10
+        };
 
     describe('FAOSTAT API Client', function () {
 
-        describe('should be configurable by the user', function () {
-            it('having "http://fenixapps2.fao.org/api/v1.0/" as default serviceUrl', function () {
+        describe('has a default base URL', function () {
+            it('http://fenixapps2.fao.org/api/v1.0/', function () {
                 expect(c.CONFIG.base_url).toEqual('http://fenixapps2.fao.org/api/v1.0/');
             });
         });
 
-        //describe("should be able to RETRIEVE data", function () {
-        //    it("from SQL databases", function () {
-        //        spyOn($, "ajax").and.callFake(function(e) {
-        //            e.success({});
-        //        });
-        //        var success_spy = jasmine.createSpy();
-        //        var error_spy = jasmine.createSpy();
-        //        w.retrieve({
-        //            payload: {
-        //                query: 'SELECT AreaCode, AreaNameE FROM Area '
-        //            },
-        //            success: success_spy,
-        //            error: error_spy
-        //        });
-        //        expect(success_spy).toHaveBeenCalled();
-        //        expect(error_spy).not.toHaveBeenCalled();
-        //    });
-        //});
+        describe('has a service for Abbreviations', function () {
+            beforeEach(function (done) {
+                c.abbreviations().then(function (response) {
+                    services.abbreviations = response;
+                    done();
+                });
+            });
+            it('that returns ' + expected.abbreviations + ' values', function () {
+                expect(services.abbreviations.data.length).toEqual(expected.abbreviations);
+            });
+        });
+
+        describe('has a service for Groups', function () {
+            beforeEach(function (done) {
+                c.groups().then(function (response) {
+                    services.groups = response;
+                    done();
+                });
+            });
+            it('that returns ' + expected.groups + ' values', function () {
+                expect(services.groups.data.length).toEqual(expected.groups);
+            });
+        });
 
     });
 
