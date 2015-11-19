4bf8cf7fe4047fb33f9d32ff02fed9739397ebd5
Kalimaha <guido.barbaglia@gmail.com>
2015-11-19 11:45:28 +0100

    Test subdimensions for codes service.

--- test/js/spec/CodesSpec.js
+++ test/js/spec/CodesSpec.js
@@ -15,27 +15,62 @@ define(['faostat-api-client'], function (FAOSTATAPIClient) {
         };
 
     describe('Codes Service', function () {
-        beforeEach(function (done) {
-            c.codes({
-                id: 'countries',
-                subcodelists: null,
-                ord: null,
-                domain_code: domain_code
-            }).then(function (response) {
-                services.codes = response.data;
-                services.metadata = response.metadata;
-                done();
+
+        describe('can fetch country codes for the ' + domain_code + ' domain', function () {
+            beforeEach(function (done) {
+                c.codes({
+                    id: 'countries',
+                    subcodelists: null,
+                    ord: null,
+                    domain_code: domain_code
+                }).then(function (response) {
+                    services.codes = response.data;
+                    services.metadata = response.metadata;
+                    done();
+                });
+            });
+            it('and returns ' + expected.codes + ' countries', function () {
+                expect(services.codes.length).toEqual(expected.codes);
+            });
+            it('and ' + expected.metadata + ' metadata parameters', function () {
+                expect(Object.keys(services.metadata.parameters).length).toEqual(expected.metadata);
+            });
+            it('and it maps "countries" to "List1Codes"', function () {
+                expect(services.metadata.parameters.parameter).toEqual('List1Codes');
             });
         });
-        it('returns ' + expected.codes + ' countries', function () {
-            expect(services.codes.length).toEqual(expected.codes);
+
+        describe('can fetch all the subdomains of "area"', function () {
+            beforeEach(function (done) {
+                c.codes({
+                    id: 'area',
+                    subcodelists: null,
+                    ord: null,
+                    domain_code: domain_code,
+                    group_subdimensions: true
+                }).then(function (response) {
+                    services.codes = response.data;
+                    services.metadata = response.metadata;
+                    done();
+                });
+            });
+            it('and returns 3 subdomains', function () {
+                expect(services.codes.length).toEqual(3);
+            });
+            it('the 1st contains ' + expected.codes + ' countries', function () {
+                expect(services.codes[0].data.length).toEqual(expected.codes);
+            });
+            it('the 2nd contains 28 regions', function () {
+                expect(services.codes[1].data.length).toEqual(28);
+            });
+            it('the 3rd contains 6 special groups', function () {
+                expect(services.codes[2].data.length).toEqual(6);
+            });
+            it('and it maps "countries" to "List1Codes"', function () {
+                expect(services.metadata.parameters.parameter).toEqual('List1Codes');
+            });
         });
-        it('it is described by 9 metadata parameters', function () {
-            expect(Object.keys(services.metadata.parameters).length).toEqual(expected.metadata);
-        });
-        it('and it maps "countries" to "List1Codes"', function () {
-            expect(services.metadata.parameters.parameter).toEqual('List1Codes');
-        });
+
     });
 
 });
